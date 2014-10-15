d99d7e1f1ed4ad0a4bc8c3037acef5a1b8ed8c95
bourbon jean-marie <mail.bourbon@gmail.com>
2014-10-15 16:06:14 +0200

    Update Simple_SCP_script.pl

--- Simple_SCP_script.pl
+++ Simple_SCP_script.pl
@@ -3,37 +3,44 @@
 use 5.010;
 use IO::File;
 use warnings;
-use strict 'vars';
 use Data::Dumper;
 use Net::OpenSSH;
+use strict 'vars';
 use Term::ANSIColor;    
 
 BEGIN:
 
 my $Pass;
-my $File  ="Malware_retro_analysis.pl";
-my $Path="/scripts/Metascan/";
+my @File =("signatures.cgi" , "Launcher.pl");
+my $Path="/var/ntop/ntop-bin/";
 
-print "\nUpdating $Path with $File file ...";
 my $PasswdFile = IO::File->new("passwd.txt",'r') || die $!;
 
-while (my $Reading = $PasswdFile->getline()) {
-	chomp($Reading);
-	$Pass=$Reading;
-}
-		
-		    
-my $ssh = Net::OpenSSH->new(
-	'10.0.0.25',
-	 user => 'root',
-	 password => $Pass,
-	 strict_mode => 0, 
-	 ctl_dir => "/tmp/.libnet-openssh-perl",
-	 );
+foreach my $File(@File){
+	
+	print "\nUpdating $Path with $File file ...";
+	while (my $Reading = $PasswdFile->getline()) {
+		chomp($Reading);
+		$Pass=$Reading;
+	}
+			
+			    
+	my $ssh = Net::OpenSSH->new(
+		'10.29.20.2',
+		 user => 'kmkz',
+		 password => $Pass,
+		 strict_mode => 0, 
+		 ctl_dir => "/tmp/.libnet-openssh-perl",
+		 );
 	 
-$ssh->scp_put({glob => 1},$File,$Path )
-	or die colored "scp failed: " . $ssh->error,'bold red';
-print colored("[OK]\n",'bold green');
+
+	
+	$ssh->scp_put({glob => 1},$File,$Path )
+		or die colored "scp failed: " . $ssh->error,'bold red';
+	
+	print colored("[OK]\n",'bold green');
+}
 say "\n[+] Repository is now up to date !\n";
+
 
 __END__
